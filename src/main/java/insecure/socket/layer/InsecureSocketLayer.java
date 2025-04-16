8474822ca866f7ed6251dffca5694bc738d907dd
LumaddT <93663941+lumaddt@users.noreply.github.com>
2025-04-16 22:40:56 +0300

    Basic socket management

---
+++ src/main/java/insecure/socket/layer/InsecureSocketLayer.java
@@ -0,0 +1,52 @@
+package insecure.socket.layer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+public class InsecureSocketLayer {
+    private static final Logger logger = LogManager.getLogger();
+
+    private static final int TIMEOUT = 1_000;
+
+    private static volatile boolean Running = false;
+
+    public static void run(int port) {
+        if (Running) {
+            logger.warn("Attempted to run, but this is already running.");
+            return;
+        }
+
+        Running = true;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            logger.info("Started on port {}.", port);
+
+            serverSocket.setSoTimeout(TIMEOUT);
+
+            while (Running) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    // TODO: Do something
+                } catch (SocketTimeoutException e) {
+                    logger.trace("Socket timed out (timeout: {}) in thread {}.", TIMEOUT, Thread.currentThread().toString());
+                }
+            }
+        } catch (IOException e) {
+            logger.fatal("An IO exception was thrown by the SocketServer. No attempt will be made to reopen the socket.\n{}\n{}", e.getMessage(), e.getStackTrace());
+        }
+    }
+
+    public static void stop() {
+        if (Running) {
+            logger.info("Stopped.");
+        } else {
+            logger.warn("Attempted to stop, but this is already stopped.");
+        }
+        Running = false;
+    }
+}
--- src/main/java/insecure/socket/layer/Main.java
+++ src/main/java/insecure/socket/layer/Main.java
@@ -1,7 +1,24 @@
 package insecure.socket.layer;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    private static final Logger logger = LogManager.getLogger();
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        logger.info("Starting up...");
+
+        Runtime.getRuntime().addShutdownHook(new Thread(Main::shutdownRoutine));
+
+        new Thread(() -> InsecureSocketLayer.run(10_008)).start();
+    }
+
+    private static void shutdownRoutine() {
+        logger.info("Shutting down...");
+
+        InsecureSocketLayer.stop();
+
+        logger.info("Have a nice day!.");
     }
 }
